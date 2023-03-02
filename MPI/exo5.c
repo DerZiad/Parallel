ec5c23dbbd7f14146b9f8b61596f38259643d31f
Ziad Bougrine <ziad.bougrine@alumni.fh-aachen.de>
2023-03-02 23:09:07 +0100

    update

--- MPI/exo5.c
+++ MPI/exo5.c
@@ -13,21 +13,17 @@ int main(int argc,char* argv[]){
 	int length = 0,receiveId,sendId,tagMessage = 1;
 	int message = procRank + 60;
 
-	for(int i = 0;i<procCount;i++){
+	sendId = procRank == (procCount - 1)?0:i+1;
 
-		if( i == procRank){
-			sendId = i == (procCount - 1)?0:i+1;
-
-			MPI_Send(&message,length,MPI_INT,sendId,tagMessage,MPI_COMM_WORLD);
+	MPI_Send(&message,length,MPI_INT,sendId,tagMessage,MPI_COMM_WORLD);
 		
-			int receiveId = i == 0? (procCount - 1) : i-1;
-			MPI_Status status;
-			int* message2 = (int*)malloc(sizeof(int));
-			MPI_Recv(message2,k,MPI_INT,receiveId,tagMessage,MPI_COMM_WORLD,&status);
-			printf("[ + ] - Message with value %d received from %d",*message,receiveId);
-			break;
-		}
-	}
+	int receiveId = procRank == 0? (procCount - 1) : i-1;
+			
+	MPI_Status status;
+			
+	int* message2 = (int*)malloc(sizeof(int));
+	MPI_Recv(message2,k,MPI_INT,receiveId,tagMessage,MPI_COMM_WORLD,&status);
+	printf("[ + ] - Message with value %d received from %d",*message2,receiveId);
 	
 	return 0;
 }
