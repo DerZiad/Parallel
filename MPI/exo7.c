2cae4b46b3acb60b21dc205fe6e64e6357e6a7d1
Ziad Bougrine <ziad.bougrine@alumni.fh-aachen.de>
2023-03-03 00:10:07 +0100

    update

--- MPI/exo7.c
+++ MPI/exo7.c
@@ -18,7 +18,7 @@ int main(int argc,char* argv[]){
 	int buf[9];
 
 	if(procRank == 0){
-		buf = {1,2,3,4,5,6,7,8,9}I[I
+		buf = {1,2,3,4,5,6,7,8,9};
 		MPI_Bcast(buf,length,MPI_INT,0,MPI_COMM_WORLD);
 	}else{
 		printf("Process %d : ",procRank);
