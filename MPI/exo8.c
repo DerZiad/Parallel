eef9de5eea24d174ca5a02b60f4d601158bbc229
Ziad Bougrine <ziad.bougrine@alumni.fh-aachen.de>
2023-03-13 00:19:05 +0100

    update

--- MPI/exo8.c
+++ MPI/exo8.c
@@ -22,12 +22,15 @@ int main(int argc,char* argv[]){
 	
 		MPI_Send(&buf,length, MPI_INT, 1, MPI_ANY_TAG,MPI_COMM_WORLD);
 	}else{
+		MPI_Status status2;
+
+		MPI_Probe(0,MPI_ANY_TAG,MPI_COMM_WORLD,&status2);
 		int count = 0;
 
 		MPI_Status status;
 
 		MPI_Get_count(&status,MPI_INT,&count);
-		
+				
 
 		MPI_Status status1;
 		int buf[count];
