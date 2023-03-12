d3951817f14ec8cfaac4468feb78a199154fa09d
Ziad Bougrine <ziad.bougrine@alumni.fh-aachen.de>
2023-03-13 00:10:21 +0100

    update

---
+++ MPI/exo8.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+int main(int argc,char* argv[]){
+	
+	int procRank,procSize;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_Size(MPI_COMM_WORLD,&procSize);
+	MPI_Comm_Rank(MPI_COMM_WORLD,&procRank);
+	
+	printf("Put array :");
+
+	if(procRank == 0){
+		int length=6;
+		int buf[length]
+		scanf("%d %d %d %d %d %d",buf,buf + 1,buf +2 , buf + 3, buf + 4,buf + 5);
+	
+	
+		MPI_Send(&buf,length, MPI_INT, 1, MPI_ANY_TAG,MPI_COMM_WORLD);
+	}else{
+		int count = 0;
+
+		MPI_Status status;
+
+		MPI_Get_count(&status,MPI_INT,&count);
+		
+
+		MPI_Status status1;
+		int buf[count];
+		MPI_Recv(&buf,count,MPI_INT,0,MPI_ANY_TAG,MPI_COMM_WORLD,&status1);
+
+		int countZeros = 0;
+
+		for(int i=0;i<count;i++){
+			if(buf[i] == 0)
+				countZeros++;
+		}
+
+		printf("Count is %d",countZeros);
+
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
+
